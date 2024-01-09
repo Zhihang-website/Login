47034dbaf7d6fcb0dbb3d26c1d642cf36557f921
Yu Zhuohang <152233019+xunhang-website@users.noreply.github.com>
2024-01-09 16:43:53 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ function validateLogin() {
     var password = document.getElementById('password').value;
 
     if ((username === '119' && password === '119')||
-        (username === '2211121225' && password === '2211121225')||
         (username === '2111111315' && password === '2111111315')||
         (username === '2210800119' && password === '2210800119')||
         (username === '2110800121' && password === '2110800121')||
-        (username === '2210700212' && password === '2210700212')||
         (username === '2111351127' && password === '2111351127')||
         (username === '2211141109' && password === '2211141109')||
         (username === '2211641225' && password === '2211641225')||
@@ -16,6 +14,28 @@ function validateLogin() {
         (username === '2310712415' && password === '2310712415'))
     {
         window.location.href = 'https://xunhang-website.github.io/Home/';
+        return false;}
+    else {
+        document.getElementById('username').value = '';
+        document.getElementById('password').value = '';
+        document.getElementById('errorMessage').innerText = '账号或密码错误';
+    }
+
+    // 余卓航
+    if (username === '2211121225' && password === '2211121225')
+    {
+        window.location.href = 'https://xunhang-website.github.io/YuZhuohang/';
+        return false;}
+    else {
+        document.getElementById('username').value = '';
+        document.getElementById('password').value = '';
+        document.getElementById('errorMessage').innerText = '账号或密码错误';
+    }
+
+    // 林安琪
+    if (username === '2210700212' && password === '2210700212')
+    {
+        window.location.href = 'https://xunhang-website.github.io/LinAnqi/';
         return false;}
     else {
         document.getElementById('username').value = '';
